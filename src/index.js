efaeab7104ddec779800b73167992d0946ba5d0d
yzzeee <goooooodgood@gmail.com>
2021-12-22 21:58:42 +0900

    Apply react and Add sample

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './style/main.css';
+import './sample/react.js';
 
 function component() {
   const element = document.createElement('div');
--- webpack.config.js
+++ webpack.config.js
@@ -30,12 +30,12 @@ module.exports = {
         },
       },
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
