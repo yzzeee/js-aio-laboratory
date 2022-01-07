6751928c96518e19829569b7bf5eac450326125c
yzzeee <goooooodgood@gmail.com>
2022-01-07 23:49:50 +0900

    Practice K8S Resource(Secret, ConfigMap) data handling

---
+++ src/dummy/k8s.js
@@ -0,0 +1,427 @@
+export const SECRET_CONFIG_MAP = [
+  {
+    metadata: {
+      name: 'basic-auth0',
+    },
+    type: 'kubernetes.io/basic-auth',
+  },
+  {
+    metadata: {
+      name: 'dockercfg0',
+    },
+    type: 'kubernetes.io/dockercfg',
+  },
+  {
+    metadata: {
+      name: 'service-account-token0',
+    },
+    type: 'kubernetes.io/service-account-token',
+  },
+  {
+    metadata: {
+      name: 'service-account-token1',
+    },
+    type: 'kubernetes.io/service-account-token',
+  },
+  {
+    metadata: {
+      name: 'dockercfg1',
+    },
+    type: 'kubernetes.io/dockercfg',
+  },
+  {
+    metadata: {
+      name: 'service-account-token2',
+    },
+    type: 'kubernetes.io/service-account-token',
+  },
+  {
+    metadata: {
+      name: 'service-account-token3',
+    },
+    type: 'kubernetes.io/service-account-token',
+  },
+  {
+    metadata: {
+      name: 'Opaque0',
+    },
+    type: 'Opaque',
+  },
+  {
+    metadata: {
+      name: 'basic-auth1',
+    },
+    type: 'kubernetes.io/basic-auth',
+  },
+  {
+    metadata: {
+      name: 'Opaque1',
+    },
+    type: 'Opaque',
+  },
+  {
+    metadata: {
+      name: 'Opaque2',
+    },
+    type: 'Opaque',
+  },
+  {
+    metadata: {
+      name: 'dockercfg2',
+    },
+    type: 'kubernetes.io/dockercfg',
+  },
+  {
+    metadata: {
+      name: 'service-account-token4',
+    },
+    type: 'kubernetes.io/service-account-token',
+  },
+  {
+    metadata: {
+      name: 'service-account-token5',
+    },
+    type: 'kubernetes.io/service-account-token',
+  },
+  {
+    metadata: {
+      name: 'dockercfg3',
+    },
+    type: 'kubernetes.io/dockercfg',
+  },
+  {
+    metadata: {
+      name: 'service-account-token6',
+    },
+    type: 'kubernetes.io/service-account-token',
+  },
+  {
+    metadata: {
+      name: 'service-account-token7',
+    },
+    type: 'kubernetes.io/service-account-token',
+  },
+  {
+    metadata: {
+      name: 'dockerconfigjson0',
+    },
+    type: 'kubernetes.io/dockerconfigjson',
+  },
+  {
+    metadata: {
+      name: 'Opaque3',
+    },
+    type: 'Opaque',
+  },
+  {
+    metadata: {
+      name: 'Opaque4',
+    },
+    type: 'Opaque',
+  },
+  {
+    metadata: {
+      name: 'Opaque5',
+    },
+    type: 'Opaque',
+  },
+  {
+    metadata: {
+      name: 'Opaque6',
+    },
+    type: 'Opaque',
+  },
+  {
+    metadata: {
+      name: 'basic-auth2',
+    },
+    type: 'kubernetes.io/basic-auth',
+  },
+  {
+    metadata: {
+      name: 'basic-auth3',
+    },
+    type: 'kubernetes.io/basic-auth',
+  },
+  {
+    metadata: {
+      name: 'dockerconfigjson1',
+    },
+    type: 'kubernetes.io/dockerconfigjson',
+  },
+  {
+    metadata: {
+      name: 'dockerconfigjson2',
+    },
+    type: 'kubernetes.io/dockerconfigjson',
+  },
+  {
+    metadata: {
+      name: 'basic-auth4',
+    },
+    type: 'kubernetes.io/basic-auth',
+  },
+  {
+    metadata: {
+      name: 'Opaque7',
+    },
+    type: 'Opaque',
+  },
+  {
+    metadata: {
+      name: 'dockerconfigjson3',
+    },
+    type: 'kubernetes.io/dockerconfigjson',
+  },
+];
+
+export const CONFIG_MAP = [
+  {
+    metadata: {
+      name: 'ConfigMap0',
+    },
+    kind: 'ConfigMap',
+  },
+  {
+    metadata: {
+      name: 'ConfigMap1',
+    },
+    kind: 'ConfigMap',
+  },
+  {
+    metadata: {
+      name: 'ConfigMap2',
+    },
+    kind: 'ConfigMap',
+  },
+  {
+    metadata: {
+      name: 'ConfigMap3',
+    },
+    kind: 'ConfigMap',
+  },
+  {
+    metadata: {
+      name: 'ConfigMap4',
+    },
+    kind: 'ConfigMap',
+  },
+  {
+    metadata: {
+      name: 'ConfigMap5',
+    },
+    kind: 'ConfigMap',
+  },
+  {
+    metadata: {
+      name: 'ConfigMap6',
+    },
+    kind: 'ConfigMap',
+  },
+];
+
+export const SECRET = [
+  {
+    metadata: {
+      name: 'basic-auth0',
+    },
+    type: 'kubernetes.io/basic-auth',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'dockercfg0',
+    },
+    type: 'kubernetes.io/dockercfg',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'service-account-token0',
+    },
+    type: 'kubernetes.io/service-account-token',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'service-account-token1',
+    },
+    type: 'kubernetes.io/service-account-token',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'dockercfg1',
+    },
+    type: 'kubernetes.io/dockercfg',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'service-account-token2',
+    },
+    type: 'kubernetes.io/service-account-token',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'service-account-token3',
+    },
+    type: 'kubernetes.io/service-account-token',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'Opaque0',
+    },
+    type: 'Opaque',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'basic-auth1',
+    },
+    type: 'kubernetes.io/basic-auth',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'Opaque1',
+    },
+    type: 'Opaque',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'Opaque2',
+    },
+    type: 'Opaque',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'dockercfg2',
+    },
+    type: 'kubernetes.io/dockercfg',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'service-account-token4',
+    },
+    type: 'kubernetes.io/service-account-token',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'service-account-token5',
+    },
+    type: 'kubernetes.io/service-account-token',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'dockercfg3',
+    },
+    type: 'kubernetes.io/dockercfg',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'service-account-token6',
+    },
+    type: 'kubernetes.io/service-account-token',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'service-account-token7',
+    },
+    type: 'kubernetes.io/service-account-token',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'dockerconfigjson0',
+    },
+    type: 'kubernetes.io/dockerconfigjson',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'Opaque3',
+    },
+    type: 'Opaque',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'Opaque4',
+    },
+    type: 'Opaque',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'Opaque5',
+    },
+    type: 'Opaque',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'Opaque6',
+    },
+    type: 'Opaque',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'basic-auth2',
+    },
+    type: 'kubernetes.io/basic-auth',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'basic-auth3',
+    },
+    type: 'kubernetes.io/basic-auth',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'dockerconfigjson1',
+    },
+    type: 'kubernetes.io/dockerconfigjson',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'dockerconfigjson2',
+    },
+    type: 'kubernetes.io/dockerconfigjson',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'basic-auth4',
+    },
+    type: 'kubernetes.io/basic-auth',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'Opaque7',
+    },
+    type: 'Opaque',
+    kind: 'Secret',
+  },
+  {
+    metadata: {
+      name: 'dockerconfigjson3',
+    },
+    type: 'kubernetes.io/dockerconfigjson',
+    kind: 'Secret',
+  },
+];
---
+++ src/playground/k8s.js
@@ -0,0 +1,74 @@
+import { CONFIG_MAP, SECRET, SECRET_CONFIG_MAP } from '../dummy/k8s';
+import { chain, concat, includes, isEqual, map, reduce } from 'lodash';
+
+const labels = {
+  'kubernetes.io/basic-auth': 'w.gitbasicauth',
+  'kubernetes.io/ssh-auth': 'SSH',
+};
+
+export const 변경전 = map(
+  reduce(
+    SECRET_CONFIG_MAP,
+    (acc, item) => {
+      const {
+        metadata: { name },
+      } = item;
+      if (includes(['kubernetes.io/basic-auth', 'kubernetes.io/ssh-auth'], item.type)) {
+        if (!acc[item.type]) {
+          acc[item.type] = [{ label: name, value: name }];
+        } else {
+          acc[item.type] = concat(acc[item.type], { label: name, value: name });
+        }
+      }
+      return acc;
+    },
+    {},
+  ),
+  (value, key) => ({
+    label: labels[key],
+    options: value,
+  }),
+);
+
+export const 변경후 = chain(SECRET_CONFIG_MAP)
+  .filter((item) => includes(['kubernetes.io/basic-auth', 'kubernetes.io/ssh-auth'], item.type))
+  .groupBy('type')
+  .map((group, type) => {
+    return { label: labels[type], options: map(group, ({ metadata: { name } }) => ({ label: name, value: name })) };
+  })
+  .value();
+
+console.log(isEqual(변경전, 변경후));
+
+export const 변경전2 = map(
+  reduce(
+    concat(CONFIG_MAP, SECRET),
+    (acc, item) => {
+      const {
+        metadata: { name },
+      } = item;
+      if (!acc[item.kind]) {
+        acc[item.kind] = [{ label: name, value: item }];
+      } else {
+        acc[item.kind] = concat(acc[item.kind], { label: name, value: item });
+      }
+      return acc;
+    },
+    {},
+  ),
+  (value, kind) => {
+    return {
+      label: kind === 'ConfigMap' ? 'ConfigMap' : 'Secret',
+      options: value,
+    };
+  },
+);
+
+export const 변경후2 = chain(concat(CONFIG_MAP, SECRET))
+  .groupBy('kind')
+  .map((group, type) => {
+    return { label: labels[type], options: map(group, (item) => ({ label: item.name, value: item })) };
+  })
+  .value();
+
+console.log(변경전2, 변경후2);
